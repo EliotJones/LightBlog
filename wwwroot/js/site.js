4d5844cebf80e94149aacdec44c65bdddf81eaa6
eliot <eliot@DESKTOP-JK4U576>
2016-11-05 14:48:29 +0000

    personalise styling and site name, add code highlighting

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,10 @@
-// Write your Javascript code.
+function AdjustWindowHeight() {
+    var requiredHeight = $(window).height() - 54 - $("footer").height() - $("nav").height();
+    $('.container').css('min-height', requiredHeight);
+}
+
+$(window).on("resize", AdjustWindowHeight);
+
+$(function () {
+    AdjustWindowHeight();
+});
